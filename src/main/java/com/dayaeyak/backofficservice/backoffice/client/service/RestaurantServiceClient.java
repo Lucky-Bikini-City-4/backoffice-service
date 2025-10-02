90adcc202af15390342c07e8d43ac14ec9cf7adf
JDG <ktkt1413@naver.com>
2025-10-02 09:54:32 +0900

    fix: kafkaProducerConfig

--- src/main/java/com/dayaeyak/backofficservice/backoffice/client/service/RestaurantServiceClient.java
+++ src/main/java/com/dayaeyak/backofficservice/backoffice/client/service/RestaurantServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name="restaurant-service", url="http://localhost:10800")
+@FeignClient(name="restaurant-service")
 public interface RestaurantServiceClient {
     @PostMapping("/restaurants")
     void registerRestaurant(
